71ae4a4e2dbd6ae298b4554c2cdba4459861d4bd
Geoff Sejai-Smith <g2_ngn@hotmail.com>
2021-09-06 21:27:05 +0100

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Geoffrey Sejai-Smith on 06/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
